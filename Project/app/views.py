a98da88af65b4d89340105a425135df53dc2ab52
Prathamesh Bhosale <prathamesh.bhosale.0201@gmail.com>
2021-10-02 13:07:58 +0530

    Got Forms working

--- Project/app/views.py
+++ Project/app/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+import pyautogui as pag
 
 # Create your views here.
 def login(request):
     if (request.method == 'POST'):
-        print(request)
+        pag.alert(text=request.POST['email']+' '+request.POST['password'], title="Request")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
     
     return render(request,'index.html')
 
