6df2eea93e6e97f5909f415679e8d8b4c86c157e
Prathamesh Bhosale <prathamesh.bhosale.0201@gmail.com>
2021-10-08 16:09:27 +0530

    Added urls and views

--- Project/app/urls.py
+++ Project/app/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('login/',views.login,name='Login'),
+    path('',views.login,name='Login'),
+    path('signup',views.signup,name='Sign Up'),
     path('<int:userId>/<str:username>/',views.home,name='Home'),
     path('listing-stay/',views.listing_stay,name='Listing Stay'),
 ]
--- Project/app/views.py
+++ Project/app/views.py
@@ -4,14 +4,17 @@ import pyautogui as pag
 # Create your views here.
 def login(request):
     if (request.method == 'POST'):
-        pag.alert(text=request.POST['email']+' '+request.POST['password'], title="Request")
-        email = request.POST.get("email")
-        password = request.POST.get("password")
+        pag.alert(text=request.POST['customer[email]']+' '+request.POST['customer[password]'], title="Request")
+        email = request.POST.get("customer[email]")
+        password = request.POST.get("customer[password]")
         user_id = 1
         username = 'test'
-        return redirect('/home/'+str(user_id)+'/'+username)
+        return redirect(str(user_id)+'/'+username)
     
-    return render(request,'index.html')
+    return render(request,'login.html')
+
+def signup(request):
+    return render(request,'signup.html')
 
 def home(request,userId, username):
     return render(request,'index.html')
