42b9702e2c57b619c20fc7ffef5c88b9b7f1a280
Kumar <kkairav1999@gmail.com>
2021-10-08 15:19:47 +0530

    added mysql password

--- booking.py
+++ booking.py
@@ -3,13 +3,13 @@ import mysql.connector
 db = mysql.connector.connect(
             host='127.0.0.1',
             username='root',
-            password='',
+            password='a1234',
             database='booking system'
         ) 
 
 cur = db.cursor()
 
-sql = "select * from user';"
+sql = "select * from user;"
 
 cur.execute(sql)
 a = cur.fetchall()
