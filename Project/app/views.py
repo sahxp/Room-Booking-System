0de047471e58f581ed51fd6b7787cbde47aedf16
Prathamesh Bhosale <prathamesh.bhosale.0201@gmail.com>
2021-10-09 08:36:22 +0530

    Added Signup sql

--- Project/app/views.py
+++ Project/app/views.py
@@ -1,22 +1,55 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
+import mysql.connector
 import pyautogui as pag
 
 # Create your views here.
 def login(request):
     if (request.method == 'POST'):
-        pag.alert(text=request.POST['customer[email]']+' '+request.POST['customer[password]'], title="Request")
         email = request.POST.get("customer[email]")
         password = request.POST.get("customer[password]")
-        user_id = 1
-        username = 'test'
-        return redirect(str(user_id)+'/'+username)
+        if (email=="" or password ==""):
+            print("Enter all Fields")
+            messages.error(request,'Enter all Fields')
+            return render(request,'login.html')
+
+        return render(request,'login.html')
     
     return render(request,'login.html')
 
 def signup(request):
     if (request.method == 'POST'):
         email = request.POST.get("customer[email]")
-        password = request.POST.get("customer[email]")
+        password = request.POST.get("customer[password]")
+        fname = request.POST.get("customer[first_name]")
+        lname = request.POST.get("customer[last_name]")
+        username = request.POST.get("customer[user_name]")
+        if (email=="" or password ==""or fname =="" or lname=="" or username==""):
+            print("Enter all Fields")
+            messages.error(request,'Enter all Fields')
+            return render(request,'signup.html')
+        else:
+            db = mysql.connector.connect(
+                    host='sql6.freemysqlhosting.net',
+                    username='sql6443052',
+                    password='8yCMcmme2d',
+                    database='sql6443052'
+                ) 
+
+            cur = db.cursor()
+
+            sql = "INSERT INTO sql6443052.User (fname, lname, email, password, username) VALUE ('"+fname+"','"+lname+"','"+email+"','"+password+"','"+username+"');"
+
+            try:
+                cur.execute(sql)
+            except:
+                messages.error(request,'Email or Username already registered')
+                return render(request,'signup.html')
+
+            db.commit()
+
+            messages.error(request,'Added the user')
+
     return render(request,'signup.html')
 
 def home(request,userId, username):
