175defc4e55569fe2d84e3dc45e4a5f73e6a7db3
Prathamesh Bhosale <prathamesh.bhosale.0201@gmail.com>
2021-10-13 14:52:16 +0530

    db testing

--- booking.py
+++ booking.py
@@ -2,16 +2,16 @@ import mysql.connector
 
 def add_user(fname,lname,email,password,username):
     db = mysql.connector.connect(
-                host='sql6.freemysqlhosting.net',
-                username='sql6443052',
-                password='8yCMcmme2d',
-                database='sql6443052'
+                host='bkswbs4odw5c3n23xb77-mysql.services.clever-cloud.com',
+                username='ucvuplrivukdot0v',
+                password='2yEAa5V6jv16Bm1d6sq7',
+                database='bkswbs4odw5c3n23xb77'
             ) 
 
     cur = db.cursor()
 
-    sql = "INSERT INTO sql6443052.User (fname, lname, email, password, username) VALUE ('"+fname+"','"+lname+"','"+email+"','"+password+"','"+username+"');"
-    #sql = "SELECT * FROM User;"
+    # sql = "INSERT INTO sql6443052.User (fname, lname, email, password, username) VALUE ('"+fname+"','"+lname+"','"+email+"','"+password+"','"+username+"');"
+    sql = "SELECT * FROM User where email='test@test.com';"
     cur.execute(sql)
     a = cur.fetchall()
     print(a)
